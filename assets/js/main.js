e8035606526e3f3a8901d8458f3c0ce3fac2953c
Anbuselvan Rocky <15264938+anburocky3@users.noreply.github.com>
2024-01-05 20:15:09 +0530

    init

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,15 @@
-// Write your javascript code here
+const ipAddressPlaceholder = document.querySelector("#ipAddress");
+// Get the ip address from this url: https://ipv4.icanhazip.com/
+
+let xhr = new XMLHttpRequest();
+
+xhr.onreadystatechange = function () {
+  if (this.readyState === 4 && this.status === 200) {
+    ipAddressPlaceholder.textContent = xhr.responseText;
+  }
+};
+xhr.open(
+  "GET",
+  "https://cors-anywhere.herokuapp.com/https://ipv4.icanhazip.com/"
+);
+xhr.send();
