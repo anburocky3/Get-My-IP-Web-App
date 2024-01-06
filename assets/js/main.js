945934e6d6165a9a0e13169fcb208127a499f5b4
Anbuselvan Rocky <15264938+anburocky3@users.noreply.github.com>
2024-01-06 13:28:16 +0530

    deploy: check for netlify

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,13 +9,13 @@ xhr.onreadystatechange = function () {
       ipAddressPlaceholder.textContent = xhr.responseText;
     } else {
       ipAddressPlaceholder.innerHTML =
-        "<div class='alert-danger'>COULDN'T GET YOUR IP. <a href='/Get-My-IP-Web-App/' class='retry-btn'>Retry</a></div>";
+        "<div class='alert-danger'>COULDN'T GET YOUR IP. <a href='/' class='retry-btn'>Retry</a></div>";
     }
   }
 };
 xhr.open(
   "GET",
   // "https://cors-anywhere.herokuapp.com/https://ipv4.icanhazip.com/"
-  "/Get-My-IP-Web-App/ip"
+  "/ip"
 );
 xhr.send();
--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,11 @@ import { defineConfig } from "vite";
 import { resolve } from "path";
 
 export default defineConfig({
-  base: "/Get-My-IP-Web-App/",
+  base: "/",
   server: {
     cors: true,
     proxy: {
-      "/Get-My-IP-Web-App/ip": {
+      "/ip": {
         target: "https://ipv4.icanhazip.com/",
         changeOrigin: true,
         secure: false,
