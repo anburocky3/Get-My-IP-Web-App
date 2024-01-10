b629ec5549b5e913032f2812ac3f326bda48d6f4
Anbuselvan Rocky <15264938+anburocky3@users.noreply.github.com>
2024-01-10 18:56:47 +0530

    wip

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,21 +3,29 @@ const ipAddressPlaceholder = document.querySelector("#ipAddress");
 
 const URL = "https://ipv4.icanhazip.com/";
 
-let xhr = new XMLHttpRequest();
-xhr.open(
-  "GET",
-  // "https://cors-anywhere.herokuapp.com/https://ipv4.icanhazip.com/"
-  URL
-);
-xhr.onreadystatechange = function () {
-  if (this.readyState === 4) {
-    if (this.status === 200) {
-      console.log(xhr.response);
-      ipAddressPlaceholder.textContent = xhr.responseText;
-    } else {
-      ipAddressPlaceholder.innerHTML =
-        "<div class='alert-danger'>COULDN'T GET YOUR IP. <a href='/' class='retry-btn'>Retry</a></div>";
-    }
-  }
-};
-xhr.send();
+// let xhr = new XMLHttpRequest();
+// xhr.open(
+//   "GET",
+//   // "https://cors-anywhere.herokuapp.com/https://ipv4.icanhazip.com/"
+//   URL
+// );
+// xhr.onreadystatechange = function () {
+//   if (this.readyState === 4) {
+//     if (this.status === 200) {
+//       console.log(xhr.response);
+//       ipAddressPlaceholder.textContent = xhr.responseText;
+//     } else {
+//       ipAddressPlaceholder.innerHTML =
+//         "<div class='alert-danger'>COULDN'T GET YOUR IP. <a href='/' class='retry-btn'>Retry</a></div>";
+//     }
+//   }
+// };
+// xhr.send();
+
+async function logIP() {
+  const response = await fetch(URL);
+  const ipAddress = await response.text();
+  console.log("fetchAPI", ipAddress);
+}
+
+logIP();
--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,4 @@ import { resolve } from "path";
 
 export default defineConfig({
   base: "/",
-  server: {
-    cors: true,
-    proxy: {
-      "/ip": {
-        target: "https://ipv4.icanhazip.com/",
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
 });
