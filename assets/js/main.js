004942259092f4bd2b83e510e62c619c0fff7415
Anbuselvan Rocky <15264938+anburocky3@users.noreply.github.com>
2024-01-10 18:49:18 +0530

    wip

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,15 @@
 const ipAddressPlaceholder = document.querySelector("#ipAddress");
 // Get the ip address from this url: https://ipv4.icanhazip.com/
 
+const URL = "https://ipv4.icanhazip.com/";
+
 let xhr = new XMLHttpRequest();
 xhr.responseType = "json";
 xhr.onreadystatechange = function () {
   if (this.readyState === 4) {
     if (this.status === 200) {
-      ipAddressPlaceholder.textContent = xhr.response.data.trim();
+      console.log(xhr.response);
+      ipAddressPlaceholder.textContent = xhr.responseText;
     } else {
       ipAddressPlaceholder.innerHTML =
         "<div class='alert-danger'>COULDN'T GET YOUR IP. <a href='/' class='retry-btn'>Retry</a></div>";
@@ -16,6 +19,6 @@ xhr.onreadystatechange = function () {
 xhr.open(
   "GET",
   // "https://cors-anywhere.herokuapp.com/https://ipv4.icanhazip.com/"
-  "/.netlify/functions/ip"
+  URL
 );
 xhr.send();
